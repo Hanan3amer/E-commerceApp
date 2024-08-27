2d2d8df6ab6cae1307306b85c119fa23503ce733
Hanan3amer <hananamer312003@gmail.com>
2024-08-27 18:25:02 +0300

    initial commit

--- src/Components/BasicModal.jsx
+++ src/Components/BasicModal.jsx
@@ -25,7 +25,7 @@ export default function BasicModal({ cartId }) {
     }
     if (data?.data?.status == 'success') {
 
-        window.location.href = '/E-commerceApp/allorders';
+        window.location.href =data?.data?.session?.url;
     }
 
     let formik = useFormik({
